1b7e6c8d9ad9b845a2cb2bea14b844fa79887567
Jasper Chan <jasperchan515@gmail.com>
2019-02-28 15:04:37 -0800

    Add series motor parameters for part 2

---
+++ Assignments/Assignment 3/series_par.m
@@ -0,0 +1,18 @@
+%   This file contains dc machine parameters 
+Lf = 40e-3; % H
+Rf = 1; % Ohms
+
+La = 80e-3; % H
+Ra = 6; % Ohms
+
+Laf = 39.5e-3; % H
+
+J = 2.12e-6; 
+Bm = 6.04e-6; % N*m*s
+
+Va = 6; % V
+
+Taf = (La + Lf)/(Ra + Rf); % s
+
+T_time = 0.5 % Step time for Tm
+Tm = 3.53e-3; % N*s
