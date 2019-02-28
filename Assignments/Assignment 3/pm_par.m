d3ee3c1467a276128777309b4499bc7b9e0dee52
Jasper Chan <jasperchan515@gmail.com>
2019-02-28 15:00:36 -0800

    Implement permanent magnet dc motor in simulink

---
+++ Assignments/Assignment 3/pm_par.m
@@ -0,0 +1,15 @@
+%   This file contains dc machine parameters 
+La = 120e-3; % H
+Ra = 7; % Ohms
+
+Kv = 1.41e-2; % V*s/rad
+
+J = 2.12e-6; 
+Bm = 6.04e-6; % N*m*s
+
+Va = 6; % V
+
+Ta = La/Ra; % s
+
+T_time = 0.5 % Step time for Tm
+Tm = 3.53e-3; % N*s
